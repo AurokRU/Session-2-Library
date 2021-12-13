11340a0646cb31ba52a172eb5171d22863f03878
Алексей Нагорный <aurok@student.nggtk.ru>
2021-12-13 21:58:33 +0400

    Добавьте файлы проекта.

---
+++ WSUniversalLib1Tests/CalculationTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WSUniversalLib1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSUniversalLib1.Tests
+{
+    [TestClass()]
+    public class CalculationTests
+    {
+        [TestMethod()]
+        public void GetQuantityForProductTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
